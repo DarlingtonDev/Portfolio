2cf64d169d278490b4bc9df24a26ee42360151d1
Darlington <obidarlington09@gmail.com>
2024-03-04 17:50:36 +0100

    Still working on responsiveness

--- src/Services.jsx
+++ src/Services.jsx
@@ -13,6 +13,101 @@ export const Service = () => {
           <br /> Your Digital Success Forward
         </p>
       </div>
+      <div className="service-sec-mobile">
+        <div className="service-boxes-mobile">
+          <div className="service-box-mobile">
+            <div className="service-box-icon-mobile">
+              <FontAwesomeIcon
+                icon={faGlobe}
+                style={{
+                  fontSize: "34px",
+                  paddingLeft: "0.4em",
+                  paddingRight: "0.4em",
+                  color: "#028a0f",
+                }}
+              />
+            </div>
+            <div className="service-box-body-mobile">
+              <h2>
+                Web and Mobile app <br />
+                development
+              </h2>
+              <p>
+                I craft user-centric web and mobile apps that bridge the gap
+                between imagination and reality.
+              </p>
+            </div>
+          </div>
+          <div className="service-box-mobile">
+            <div className="service-box-icon-mobile">
+              <FontAwesomeIcon
+                icon={faSass}
+                style={{
+                  fontSize: "34px",
+                  paddingLeft: "0.4em",
+                  paddingRight: "0.4em",
+                  color: "#028a0f",
+                }}
+              />
+            </div>
+            <div className="service-box-body-mobile">
+              <h2>
+                Building SAAS <br />
+                Products
+              </h2>
+              <p>
+                Building and shipping ideas, from features to complete products
+              </p>
+            </div>
+          </div>
+          <div className="service-box-mobile">
+            <div className="service-box-icon-mobile">
+              <FontAwesomeIcon
+                icon={faShop}
+                style={{
+                  fontSize: "34px",
+                  paddingLeft: "0.4em",
+                  paddingRight: "0.4em",
+                  color: "#028a0f",
+                }}
+              />
+            </div>
+            <div className="service-box-body-mobile">
+              <h2>
+                E-commerce <br />
+                Solutions
+              </h2>
+              <p>
+                Developing custom e-commerce platforms or integrating with
+                existing solutions like Shopify or WooCommerce
+              </p>
+            </div>
+          </div>
+          <div className="service-box-mobile">
+            <div className="service-box-icon-mobile">
+              <FontAwesomeIcon
+                icon={faHammer}
+                style={{
+                  fontSize: "34px",
+                  paddingLeft: "0.4em",
+                  paddingRight: "0.4em",
+                  color: "#028a0f",
+                }}
+              />
+            </div>
+            <div className="service-box-body-mobile">
+              <h2>
+                Maintenance and <br /> Support
+              </h2>
+              <p>
+                Providing ongoing maintenance, updates, and technical support
+                services to ensure the smooth operation and performance of
+                websites and applications post-launch.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="services-sec">
         <div className="service-div">
           <div className="dots"></div>
